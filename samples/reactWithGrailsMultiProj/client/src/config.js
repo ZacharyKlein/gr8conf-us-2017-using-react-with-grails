5b30bdb69c1d536c2d26d4de7297b868d669890d
Zachary Klein <zak@silver-chalice.com>
2017-07-27 21:56:04 -0500

    removing older version of multi-proj

--- samples/reactWithGrailsMultiProj/client/src/config.js
+++
@@ -1,5 +0,0 @@
-import pjson from './../package.json';
-
-export const SERVER_URL = 'http://localhost:8050/';
-export const CLIENT_VERSION = pjson.version;
-export const REACT_VERSION = pjson.dependencies.react;
